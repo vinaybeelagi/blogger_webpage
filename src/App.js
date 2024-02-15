9c0456a74312c6030c5e0215d158e32a2dd3aac2
vinaybeelagi <vinaykumar.b@neoito.com>
2024-02-15 14:14:13 +0530

    html skeleton part is added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/register" element={<RegisterPage />} /> 
+          <Route path="/register" element={<RegisterPage />} />   
           {/* Add other routes as needed */}
         </Routes>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,18 @@
-// Home.js
-
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Home.css';
+import React from 'react'
+import './Home.css'
 
 const Home = () => {
-  const [visibility, setVisibility] = useState('public');
-
-  const handleVisibilityChange = (vis) => {
-    setVisibility(vis);
-  };
-
   return (
-    <div>
-      <header>
-        <h1>Your Blog Name</h1>
-        <nav>
-          <ul>
-            <li><Link to="/register">Register</Link></li>
-            {/* Add other navigation buttons here */}
-            <li><button onClick={() => console.log("Login clicked")}>Login</button></li>
-          </ul>
-        </nav>
-      </header>
-      <div className="container">
-        <h2>Content Visibility</h2>
-        <div>
-          <button onClick={() => handleVisibilityChange('public')}>Public</button>
-          <button onClick={() => handleVisibilityChange('private')}>Private</button>
+    <div className="App">
+      <div className='header'> Header</div>
+      <div className='container'>
+        <div className='nav'> Navbar</div>
+        <div className='main'>
+          <div className='left-main'>left-main</div>
+          <div className='right-main'>right-main'</div>
         </div>
-      </div>
-      <div className="container">
-        <h2>Content</h2>
-        <p>No content to display.</p>
       </div>
     </div>
   );
-};
-
-export default Home;
+}
+export default Home
